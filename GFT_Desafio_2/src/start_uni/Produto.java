2fac93d197661f098d74c359f0547f4ba37640fb
FelipeBelarmino <70665848+FelipeBelarmino@users.noreply.github.com>
2021-02-01 11:02:45 -0300

    class videogame

--- GFT_Desafio_2/src/start_uni/Produto.java
+++ GFT_Desafio_2/src/start_uni/Produto.java
@@ -1,6 +1,8 @@
 package start_uni;
 
 public class Produto {
+    String modelo;
+    Boolean usado;
     String nome;
     double preco;
     int qtd;
@@ -9,7 +11,14 @@ public class Produto {
 
     void status() {
         System.out.println("Produto: "+this.nome);
+        System.out.println("Tema: "+this.tema);
+        System.out.println("Autor: "+this.autor);
         System.out.println("Preço: "+this.preco);
+        System.out.println("Quantidade: "+this.qtd);
+    }
+    void statusGame() {
+        System.out.println("Produto: "+this.nome);
+        System.out.println("Usado: "+this.usado);
         System.out.println("Quantidade: "+this.qtd);
     }
 }
--- GFT_Desafio_2/src/start_uni/VideoGame.java
+++ GFT_Desafio_2/src/start_uni/VideoGame.java
@@ -6,6 +6,15 @@ public class VideoGame {
         Scanner scan = new Scanner(System.in);
         Produto videoGame = new Produto();
 
+        System.out.println("Entre com o nome do game:");
+        videoGame.nome = scan.nextLine();
 
+        System.out.println("É usado? false ou true:");
+        videoGame.usado = Boolean.valueOf(scan.nextLine());
+
+        System.out.println("Entre com a quantidade:");
+        videoGame.qtd = scan.nextInt();
+
+        videoGame.statusGame();
     }
 }
